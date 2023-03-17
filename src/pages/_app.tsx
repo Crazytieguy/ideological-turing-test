5637bf14947454e4a37a895278fcbafc6b10b8e8
Yoav Tzfati <yoav.tzfati@gmail.com>
2023-03-17 21:49:04 +0200

    try without getinitialprops

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/global.css';
 import type { Session } from 'next-auth';
-import { getSession, SessionProvider } from 'next-auth/react';
+import { SessionProvider } from 'next-auth/react';
 import type { AppType } from 'next/app';
 import { trpc } from 'utils/trpc';
 
@@ -15,10 +15,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
   );
 };
 
-MyApp.getInitialProps = async ({ ctx }) => {
-  return {
-    session: await getSession(ctx),
-  };
-};
+// MyApp.getInitialProps = async ({ ctx }) => {
+//   return {
+//     session: await getSession(ctx),
+//   };
+// };
 
 export default trpc.withTRPC(MyApp);
